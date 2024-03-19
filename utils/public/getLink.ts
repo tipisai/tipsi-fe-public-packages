34fee977c8c2eebf2b2ab551e59b5f301fc7a4a6
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-03-19 15:54:02 +0800

    feat: ✨ update get links

--- utils/public/getLink.ts
+++ utils/public/getLink.ts
@@ -1,16 +1,5 @@
 import { isCloudVersion, isServerRender } from "./typeHelper"
 
-export const getILLABuilderURL = (customDomain?: string): string => {
-  if (customDomain) {
-    return `https://${customDomain}/builder`
-  }
-  if (process.env.ILLA_BUILDER_URL || isCloudVersion || isServerRender) {
-    return `${process.env.ILLA_BUILDER_URL}`
-  } else {
-    return `${location.origin}/build`
-  }
-}
-
 export const getILLACloudURL = (customDomain?: string): string => {
   if (customDomain) {
     return `https://${customDomain}/cloud`
@@ -20,13 +9,6 @@ export const getILLACloudURL = (customDomain?: string): string => {
   } else {
     return `${location.origin}/cloud`
   }
-}
-
-export const getILLAFlowURL = (customDomain?: string): string => {
-  if (customDomain) {
-    return `https://${customDomain}/flow`
-  }
-  return `${process.env.ILLA_FLOW_URL}`
 }
 
 export const getMarketLinkTemplate = (appID: string): string => {
@@ -42,12 +24,4 @@ export const getPublicLinkTemplate = (
 
 export const getAgentPublicLink = (agentID: string): string => {
   return `${process.env.ILLA_MARKET_URL}/ai-agent/${agentID}/detail`
-}
-
-export const getFlowEditLink = (
-  teamIdentify: string,
-  flowID: string,
-  customDomain?: string,
-): string => {
-  return `${getILLAFlowURL(customDomain)}/${teamIdentify}/flow/${flowID}`
 }
