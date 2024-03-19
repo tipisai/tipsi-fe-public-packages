ada32e90a0a8a8c03e94090f288b3b19a61a55bd
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-03-19 19:42:21 +0800

    feat: ✨ remove track

--- utils/MixpanelUtils/index.tsx
+++ utils/MixpanelUtils/index.tsx
@@ -1,6 +1,6 @@
 import * as amplitude from "@amplitude/analytics-browser"
-import { isServerRender } from "@illa-public/utils"
 import mixpanel from "mixpanel-browser"
+import { isServerRender } from "@illa-public/utils"
 import {
   ILLAProperties,
   ILLA_MIXPANEL_EVENT_TYPE,
@@ -22,37 +22,37 @@ class ILLAMixpanelTools {
       !!process.env.ILLA_AMPLITUDE_API_KEY &&
       !isServerRender
     if (this.enable) {
-      mixpanel.init(process.env.ILLA_MIXPANEL_API_KEY as string, {
-        debug: process.env.ILLA_APP_ENV === "development",
-        test: process.env.ILLA_APP_ENV !== "production",
-        ignore_dnt: process.env.ILLA_APP_ENV === "development",
-        loaded(mixpanelProto) {
-          const originalTrack = mixpanelProto.track
-          mixpanelProto.track = function (event, properties) {
-            originalTrack.call(mixpanelProto, event, {
-              ...properties,
-              environment: process.env.ILLA_APP_ENV,
-              fe_version_code: process.env.ILLA_APP_VERSION,
-            })
-          }
-        },
-      })
-      amplitude.init(process.env.ILLA_AMPLITUDE_API_KEY as string, undefined, {
-        logLevel:
-          process.env.ILLA_APP_ENV === "development"
-            ? amplitude.Types.LogLevel.Debug
-            : amplitude.Types.LogLevel.None,
-      })
+      // mixpanel.init(process.env.ILLA_MIXPANEL_API_KEY as string, {
+      //   debug: process.env.ILLA_APP_ENV === "development",
+      //   test: process.env.ILLA_APP_ENV !== "production",
+      //   ignore_dnt: process.env.ILLA_APP_ENV === "development",
+      //   loaded(mixpanelProto) {
+      //     const originalTrack = mixpanelProto.track
+      //     mixpanelProto.track = function (event, properties) {
+      //       originalTrack.call(mixpanelProto, event, {
+      //         ...properties,
+      //         environment: process.env.ILLA_APP_ENV,
+      //         fe_version_code: process.env.ILLA_APP_VERSION,
+      //       })
+      //     }
+      //   },
+      // })
+      // amplitude.init(process.env.ILLA_AMPLITUDE_API_KEY as string, undefined, {
+      //   logLevel:
+      //     process.env.ILLA_APP_ENV === "development"
+      //       ? amplitude.Types.LogLevel.Debug
+      //       : amplitude.Types.LogLevel.None,
+      // })
     }
   }
 
   public async setDeviceID() {
     if (this.enable) {
-      const deviceID = await getDeviceUUID()
-      mixpanel.register({
-        ILLA_device_ID: deviceID,
-      })
-      amplitude.setDeviceId(deviceID)
+      // const deviceID = await getDeviceUUID()
+      // mixpanel.register({
+      //   ILLA_device_ID: deviceID,
+      // })
+      // amplitude.setDeviceId(deviceID)
     }
   }
 
@@ -65,61 +65,61 @@ class ILLAMixpanelTools {
 
   public track(event: ILLA_MIXPANEL_EVENT_TYPE, properties: ILLAProperties) {
     if (this.enable) {
-      mixpanel.track(event, {
-        ...properties,
-      })
-      amplitude.track(event, {
-        ...properties,
-        environment: process.env.ILLA_APP_ENV,
-        fe_version_code: process.env.ILLA_APP_VERSION,
-      })
+      // mixpanel.track(event, {
+      //   ...properties,
+      // })
+      // amplitude.track(event, {
+      //   ...properties,
+      //   environment: process.env.ILLA_APP_ENV,
+      //   fe_version_code: process.env.ILLA_APP_VERSION,
+      // })
     }
   }
 
   public setGroup(teamIdentifier: string | string[]) {
     if (this.enable) {
-      mixpanel.set_group("team", teamIdentifier)
-      amplitude.setGroup("team", teamIdentifier)
+      // mixpanel.set_group("team", teamIdentifier)
+      // amplitude.setGroup("team", teamIdentifier)
     }
   }
 
   public setUserID(userID: string) {
     if (this.enable) {
-      mixpanel.identify(userID)
-      amplitude.setUserId(userID)
+      // mixpanel.identify(userID)
+      // amplitude.setUserId(userID)
     }
   }
 
   public setUserProperties(properties: Record<string, any>) {
     if (this.enable) {
-      mixpanel.people.set(properties)
-      const identifyEvent = new amplitude.Identify()
-      for (let propertiesKey in properties) {
-        identifyEvent.set(propertiesKey, properties[propertiesKey])
-      }
-      amplitude.identify(identifyEvent)
+      // mixpanel.people.set(properties)
+      // const identifyEvent = new amplitude.Identify()
+      // for (let propertiesKey in properties) {
+      //   identifyEvent.set(propertiesKey, properties[propertiesKey])
+      // }
+      // amplitude.identify(identifyEvent)
     }
   }
 
   public reset() {
     if (this.enable) {
-      mixpanel.reset()
-      amplitude.reset()
+      // mixpanel.reset()
+      // amplitude.reset()
     }
   }
 
   public pageTimeEvent() {
     if (this.enable) {
-      mixpanel.time_event("page_duration")
+      // mixpanel.time_event("page_duration")
     }
   }
 
   public trackTimeEvent(pageName: ILLA_PAGE_NAME, teamIdentifier: string) {
     if (this.enable) {
-      mixpanel.track("page_duration", {
-        page: pageName,
-        team_id: teamIdentifier,
-      })
+      // mixpanel.track("page_duration", {
+      //   page: pageName,
+      //   team_id: teamIdentifier,
+      // })
     }
   }
 }
