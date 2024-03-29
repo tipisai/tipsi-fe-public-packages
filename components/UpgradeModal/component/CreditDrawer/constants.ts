285c8c03b3b7867fa3283d0c0bff182d03338937
Scenery <1507337624@qq.com>
2024-03-29 20:32:40 +0800

    chore: update translation

--- components/UpgradeModal/component/CreditDrawer/constants.ts
+++ components/UpgradeModal/component/CreditDrawer/constants.ts
@@ -2,17 +2,17 @@ export const LEARN_MORE_LINK =
   "https://builder.illacloud.com/illa_policy/deploy/app/ILAex4p1C7sk"
 
 export enum CREDIT_MORE_TEXT {
-  SUBSCRIBE = "billing.payment_sidebar.description_title.colla_sub",
-  ADD_CREDIT = "billing.payment_sidebar.description_title.colla_increase",
-  REMOVE_CREDIT = "billing.payment_sidebar.description_title.colla_remove",
-  CANCEL_SUBSCRIPTION = "billing.payment_sidebar.description_title.colla_unsubscribe",
-  MONTH_YEAR_UPDATE = "billing.payment_sidebar.description_title.colla_update",
+  SUBSCRIBE = "tipi_billing.sub_desc",
+  ADD_CREDIT = "tipi_billing.increase_desc",
+  REMOVE_CREDIT = "tipi_billing.reduce_desc",
+  CANCEL_SUBSCRIPTION = "tipi_billing.unsub_desc",
+  MONTH_YEAR_UPDATE = "tipi_billing.change_desc",
 }
 
 export enum CREDIT_BUTTON_TEXT {
-  SUBSCRIBE = "billing.payment_sidebar.button.subscribe",
-  ADD_CREDIT = "billing.payment_sidebar.button.colla_increase",
-  REMOVE_CREDIT = "billing.payment_sidebar.button.colla_remove",
-  CANCEL_SUBSCRIPTION = "billing.payment_sidebar.button.unsubscribe",
-  MONTH_YEAR_UPDATE = "billing.payment_sidebar.button.change_plan",
+  SUBSCRIBE = "tipi_billing.subscribe",
+  ADD_CREDIT = "tipi_billing.increase",
+  REMOVE_CREDIT = "tipi_billing.remove",
+  CANCEL_SUBSCRIPTION = "tipi_billing.unsubscribe",
+  MONTH_YEAR_UPDATE = "tipi_billing.change_plan",
 }
