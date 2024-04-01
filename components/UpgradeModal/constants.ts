609fd04179aabc6c8d078704687b551b75c8cf03
Scenery <1507337624@qq.com>
2024-04-01 19:51:55 +0800

    chore: update billing translate

---
+++ components/UpgradeModal/constants.ts
@@ -0,0 +1,14 @@
+export enum BILLING_REPORT_FROM {
+  CREATE = "create",
+  EDIT = "edit",
+  RUN = "run",
+  SETTINGS = "settings",
+}
+
+export enum BILLING_REPORT_TYPE {
+  INCREASE = "increase",
+  REMOVE = "remove",
+  SUBSCRIBE = "subscribe",
+  UNSUBSCRIBE = "unsubscribe",
+  CHANGE_PLAN = "change_plan",
+}
