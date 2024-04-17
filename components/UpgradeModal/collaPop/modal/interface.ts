c53f53fd07c7faf4d3ad0ec1fcfda62282f2921f
Scenery <1507337624@qq.com>
2024-04-17 15:27:31 +0800

    feat: remove credit utils params

--- components/UpgradeModal/collaPop/modal/interface.ts
+++ components/UpgradeModal/collaPop/modal/interface.ts
@@ -1,9 +1,7 @@
 import { BILLING_REPORT_FROM } from "../../constants"
-import { CreditModalType } from "../../interface"
 
 export interface ModalShowProps {
   from: BILLING_REPORT_FROM
   visible?: boolean
   id?: string
-  modalType: CreditModalType
 }
