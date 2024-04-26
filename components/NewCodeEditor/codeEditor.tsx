3b163c834680b48ba6c0b84459e1f92d8eceda9f
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-04-26 13:24:00 +0800

    feat: ✨ update codeEditor

--- components/NewCodeEditor/codeEditor.tsx
+++ components/NewCodeEditor/codeEditor.tsx
@@ -1,5 +1,4 @@
-import { debounce } from "lodash-es"
-import { FC, useCallback, useMemo, useRef, useState } from "react"
+import { forwardRef, useCallback, useMemo, useState } from "react"
 import {
   getStringSnippets,
   isDynamicStringSnippet,
@@ -13,105 +12,106 @@ import OpenWindowIcon from "./assets/openWindow.svg?react"
 import { ICodeEditorProps } from "./interface"
 import { ILLACodeMirrorWrapperStyle, openWindowIconHotspotStyle } from "./style"
 
-const CodeEditor: FC<ICodeEditorProps> = (props) => {
-  const {
-    value = "",
-    onChange = () => {},
-    placeholder,
-    styles,
-    options,
-    editable = true,
-    readOnly,
-    wrapperCss,
-    completionOptions,
-    onBlur = () => {},
-    onFocus = () => {},
-    canExpand,
-    modalTitle = "",
-  } = props
+const CodeEditor = forwardRef<HTMLDivElement, ICodeEditorProps>(
+  (props: ICodeEditorProps, ref) => {
+    const {
+      value = "",
+      onChange = () => {},
+      placeholder,
+      styles,
+      options,
+      editable = true,
+      readOnly,
+      wrapperCss,
+      completionOptions,
+      onBlur = () => {},
+      onFocus = () => {},
+      canExpand,
+      modalTitle = "",
+    } = props
 
-  const [isExpanded, setIsExpanded] = useState(false)
+    const [isExpanded, setIsExpanded] = useState(false)
 
-  const innerCanExpand = canExpand && !readOnly && editable
+    const innerCanExpand = canExpand && !readOnly && editable
 
-  const needExecuteCode = value
+    const needExecuteCode = value
 
-  const stringSnippets = useMemo(() => {
-    const result: IExpressionShape[] = []
-    const dynamicStrings = getStringSnippets(needExecuteCode)
-    dynamicStrings.forEach((stringSnippet) => {
-      if (isDynamicStringSnippet(stringSnippet)) {
-        const currentKey = stringSnippet.split("{{")[1].split("}}")[0].trim()
+    const stringSnippets = useMemo(() => {
+      const result: IExpressionShape[] = []
+      const dynamicStrings = getStringSnippets(needExecuteCode)
+      dynamicStrings.forEach((stringSnippet) => {
+        if (isDynamicStringSnippet(stringSnippet)) {
+          const currentKey = stringSnippet.split("{{")[1].split("}}")[0].trim()
 
-        if (
-          !currentKey ||
-          completionOptions.find((item) => item.key.trim() === currentKey)
-        ) {
-          result.push({ hasError: false, value: stringSnippet })
-        } else {
-          result.push({ hasError: true, value: stringSnippet })
+          if (
+            !currentKey ||
+            completionOptions.find((item) => item.key.trim() === currentKey)
+          ) {
+            result.push({ hasError: false, value: stringSnippet })
+          } else {
+            result.push({ hasError: true, value: stringSnippet })
+          }
         }
-      }
-    })
-    return result
-  }, [completionOptions, needExecuteCode])
+      })
+      return result
+    }, [completionOptions, needExecuteCode])
 
-  const hasError = stringSnippets.some((item) => item.hasError)
-  const containerRef = useRef<HTMLDivElement>(null)
+    const hasError = stringSnippets.some((item) => item.hasError)
 
-  const debounceHandleChange = useMemo(() => {
-    return debounce(onChange, 160)
-  }, [onChange])
+    const handleOpenExpandModal = useCallback(() => {
+      setIsExpanded(true)
+    }, [])
 
-  const handleOpenExpandModal = useCallback(() => {
-    setIsExpanded(true)
-  }, [])
-
-  return (
-    <div
-      css={[ILLACodeMirrorWrapperStyle, wrapperCss, illaCodeMirrorTooltipStyle]}
-      ref={containerRef}
-    >
-      <ILLACodeMirrorCore
-        placeholder={placeholder}
-        value={fixedValue(value)}
-        onChange={debounceHandleChange}
-        styles={styles}
-        expressions={stringSnippets}
-        options={options}
-        editable={editable}
-        readOnly={readOnly}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        completionOptions={completionOptions}
-        hasError={hasError}
-      />
-      {innerCanExpand && (
-        <div
-          css={openWindowIconHotspotStyle}
-          className="open-window-icon-hotspot"
-          onClick={handleOpenExpandModal}
-        >
-          <OpenWindowIcon />
-        </div>
-      )}
-      <ModalCodeMirror
-        title={modalTitle}
-        expand={isExpanded}
-        setExpand={setIsExpanded}
-        codeMirrorOptions={{
-          value: fixedValue(value),
-          onChange: onChange,
-          lang: options?.lang,
-          placeholder: placeholder,
-          onBlur: onBlur,
-          onFocus: onFocus,
-          completionOptions: completionOptions,
-        }}
-      />
-    </div>
-  )
-}
+    return (
+      <div
+        css={[
+          ILLACodeMirrorWrapperStyle,
+          wrapperCss,
+          illaCodeMirrorTooltipStyle,
+        ]}
+        ref={ref}
+      >
+        <ILLACodeMirrorCore
+          placeholder={placeholder}
+          value={fixedValue(value)}
+          onChange={onChange}
+          styles={styles}
+          expressions={stringSnippets}
+          options={options}
+          editable={editable}
+          readOnly={readOnly}
+          onBlur={onBlur}
+          onFocus={onFocus}
+          completionOptions={completionOptions}
+          hasError={hasError}
+        />
+        {innerCanExpand && (
+          <div
+            css={openWindowIconHotspotStyle}
+            className="open-window-icon-hotspot"
+            onClick={handleOpenExpandModal}
+          >
+            <OpenWindowIcon />
+          </div>
+        )}
+        <ModalCodeMirror
+          title={modalTitle}
+          expand={isExpanded}
+          setExpand={setIsExpanded}
+          codeMirrorOptions={{
+            value: fixedValue(value),
+            onChange: onChange,
+            lang: options?.lang,
+            placeholder: placeholder,
+            onBlur: onBlur,
+            onFocus: onFocus,
+            completionOptions: completionOptions,
+          }}
+        />
+      </div>
+    )
+  },
+)
 
 CodeEditor.displayName = "CodeEditor"
 export default CodeEditor
