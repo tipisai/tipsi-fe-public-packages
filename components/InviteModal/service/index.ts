29789fa1dc4dc447258b8c453c5164dc69d8e906
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-05-07 17:33:48 +0800

    feat: 🔥 remove invite modal

--- components/InviteModal/service/index.ts
+++
@@ -1,9 +0,0 @@
-import { publicHashtagRequest } from "@illa-public/illa-net"
-import { FuzzySearchHashTag } from "./interface"
-
-export const fetchFuzzySearchHashTag = (keyword: string) => {
-  return publicHashtagRequest<FuzzySearchHashTag>({
-    method: "GET",
-    url: `/search?keyword=${keyword}`,
-  })
-}
